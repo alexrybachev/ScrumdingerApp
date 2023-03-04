bd2dc9f3299979bc3c2500aa0aae1cfeaeb5add5
Alex Rybachev <rybachev.as@gmail.com>
2023-03-04 23:41:59 +0400

    Transcribing speech to text

--- ScrumdingerApp/MeetingViews/MeetingView.swift
+++ ScrumdingerApp/MeetingViews/MeetingView.swift
@@ -11,6 +11,8 @@ import AVFoundation
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording = false
     
     private var player: AVPlayer { AVPlayer.sharedDingPlayer }
     
@@ -25,6 +27,7 @@ struct MeetingView: View {
                                   theme: scrum.theme)
                                 
                 MeetingTimerView(speakers: scrumTimer.speakers,
+                                 isRecording: isRecording,
                                  theme: scrum.theme)
                 
                 MeetingFooterView(speakers: scrumTimer.speakers,
@@ -40,12 +43,18 @@ struct MeetingView: View {
                 player.seek(to: .zero)
                 player.play()
             }
+            speechRecognizer.reset()
+            speechRecognizer.transcribe()
+            isRecording = true
             scrumTimer.startScrum()
         }
         .onDisappear {
             scrumTimer.stopScrum()
+            speechRecognizer.stopTranscribing()
+            isRecording = false
             let newHistory = History(attendees: scrum.attendees,
-                                     lengthInMinutes: scrum.timer.secondsElapsed / 60)
+                                     lengthInMinutes: scrum.timer.secondsElapsed / 60,
+                                     transcript: speechRecognizer.transcript)
             scrum.history.insert(newHistory, at: 0)
         }
         .navigationBarTitleDisplayMode(.inline)
