e54ea91ab51e168333c8fff6e0c1ccece948e6f1
Alex Rybachev <rybachev.as@gmail.com>
2023-03-04 12:08:04 +0400

    Displaying data in a list

--- ScrumdingerApp/ScrumdingerAppApp.swift
+++ ScrumdingerApp/ScrumdingerAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ScrumdingerAppApp: App {
     var body: some Scene {
         WindowGroup {
-            MeetingView()
+            ScrumsView(scrums: DailyScrum.sampleData)
         }
     }
 }
---
+++ ScrumdingerApp/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  ScrumdingerApp
+//
+//  Created by Aleksandr Rybachev on 04.03.2023.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
