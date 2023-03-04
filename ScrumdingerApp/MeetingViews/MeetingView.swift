304f063d3099f71a978607addf2505af0dedbea6
Alex Rybachev <rybachev.as@gmail.com>
2023-03-04 20:26:32 +0400

    Managing state and life cycle

---
+++ ScrumdingerApp/MeetingViews/MeetingView.swift
@@ -0,0 +1,56 @@
+//
+//  MeetingView.swift
+//  ScrumdingerApp
+//
+//  Created by Aleksandr Rybachev on 04.03.2023.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(scrum.theme.mainColor)
+            
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed,
+                                  secondsRemaining: scrumTimer.secondsRemaining,
+                                  theme: scrum.theme)
+                                
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                
+                MeetingFooterView(speakers: scrumTimer.speakers,
+                                  skipAction: scrumTimer.skipSpeaker)
+            }
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes,
+                             attendees: scrum.attendees)
+            scrumTimer.speakerChangedAction = {
+                player.seek(to: .zero)
+                player.play()
+            }
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+    }
+}
